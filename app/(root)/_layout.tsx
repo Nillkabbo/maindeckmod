0df202e13c2bd23c9568bfcd292455ed18c0d415
MD. RAKIBUL HASAN <aporbo.hasan@gmail.com>
2024-11-17 12:02:34 -0500

    Added route

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tab)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+};
+
+export default Layout;
