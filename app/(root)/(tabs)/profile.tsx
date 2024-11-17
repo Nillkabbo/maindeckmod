7d9b91dde8e159bcf5c680e660dcab2b3ffea107
MD. RAKIBUL HASAN <aporbo.hasan@gmail.com>
2024-11-17 11:31:47 -0500

    first commit

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text className="text-black text-center text-2xl">Profile Page</Text>
+      <StatusBar style="dark" />
+    </SafeAreaView>
+  );
+};
+export default Profile;
