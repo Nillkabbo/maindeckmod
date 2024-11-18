d402c4ddcf6d363b4ba7e0867b4fe0e439277def
MD. RAKIBUL HASAN <aporbo.hasan@gmail.com>
2024-11-17 19:54:20 -0500

    development build

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,7 +1,7 @@
 import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Welcome = () => {
@@ -11,9 +11,17 @@ const Welcome = () => {
         onPress={() => {
           router.replace("/(auth)/sign-up");
         }}
-        className="w-full flex justify-end items-end p-5"
+        className="w-full flex justify-end items-end p-5 bg-general-400"
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
+        <View>
+          <Text className="text-black text-center text-2xl font-JakartaBold">
+            Welcome to Expo
+          </Text>
+          <Text className="text-black text-center text-md font-Jakarta">
+            Let's get started
+          </Text>
+        </View>
       </TouchableOpacity>
       <StatusBar style="dark" />
     </SafeAreaView>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 // Import your global CSS file
+import "expo-dev-client";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
